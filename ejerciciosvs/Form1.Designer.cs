e50af946d3f1b39d1812be8e6faabb3e76ffd0b7
Cristopher Alejandro Bernal Chavez <USSS027423@ugb.edu.sv>
2023-08-29 15:20:26 -0600

    Cálculo de impuestaos  a las Actividades Económicas

--- ejerciciosvs/Form1.Designer.cs
+++ ejerciciosvs/Form1.Designer.cs
@@ -29,46 +29,71 @@ namespace ejerciciosvs
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtEdad = new System.Windows.Forms.MaskedTextBox();
-            this.lblEdad = new System.Windows.Forms.Label();
-            this.btnEdan = new System.Windows.Forms.Button();
+            this.lblTituloImouesto = new System.Windows.Forms.Label();
+            this.lblCantidadImpuesto = new System.Windows.Forms.Label();
+            this.btnCalcularImpuesto = new System.Windows.Forms.Button();
+            this.txtCantidadImpuesto = new System.Windows.Forms.TextBox();
+            this.lblValorAPagarImpuesto = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // txtEdad
+            // lblTituloImouesto
             // 
-            this.txtEdad.Location = new System.Drawing.Point(149, 69);
-            this.txtEdad.Name = "txtEdad";
-            this.txtEdad.Size = new System.Drawing.Size(100, 20);
-            this.txtEdad.TabIndex = 0;
+            this.lblTituloImouesto.AutoSize = true;
+            this.lblTituloImouesto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTituloImouesto.Location = new System.Drawing.Point(123, 9);
+            this.lblTituloImouesto.Name = "lblTituloImouesto";
+            this.lblTituloImouesto.Size = new System.Drawing.Size(442, 20);
+            this.lblTituloImouesto.TabIndex = 0;
+            this.lblTituloImouesto.Text = "Cálculo de impuestaos  a las Actividades Económicas ";
             // 
-            // lblEdad
+            // lblCantidadImpuesto
             // 
-            this.lblEdad.AutoSize = true;
-            this.lblEdad.Location = new System.Drawing.Point(66, 69);
-            this.lblEdad.Name = "lblEdad";
-            this.lblEdad.Size = new System.Drawing.Size(32, 13);
-            this.lblEdad.TabIndex = 1;
-            this.lblEdad.Text = "Edad";
-            this.lblEdad.Click += new System.EventHandler(this.lblEdad_Click);
+            this.lblCantidadImpuesto.AutoSize = true;
+            this.lblCantidadImpuesto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCantidadImpuesto.Location = new System.Drawing.Point(160, 84);
+            this.lblCantidadImpuesto.Name = "lblCantidadImpuesto";
+            this.lblCantidadImpuesto.Size = new System.Drawing.Size(86, 20);
+            this.lblCantidadImpuesto.TabIndex = 1;
+            this.lblCantidadImpuesto.Text = "Cantidad:";
             // 
-            // btnEdan
+            // btnCalcularImpuesto
             // 
-            this.btnEdan.Location = new System.Drawing.Point(149, 121);
-            this.btnEdan.Name = "btnEdan";
-            this.btnEdan.Size = new System.Drawing.Size(115, 32);
-            this.btnEdan.TabIndex = 2;
-            this.btnEdan.Text = "Comprobar";
-            this.btnEdan.UseVisualStyleBackColor = true;
-            this.btnEdan.Click += new System.EventHandler(this.btnEdan_Click);
+            this.btnCalcularImpuesto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcularImpuesto.Location = new System.Drawing.Point(381, 76);
+            this.btnCalcularImpuesto.Name = "btnCalcularImpuesto";
+            this.btnCalcularImpuesto.Size = new System.Drawing.Size(95, 31);
+            this.btnCalcularImpuesto.TabIndex = 2;
+            this.btnCalcularImpuesto.Text = "Calcular";
+            this.btnCalcularImpuesto.UseVisualStyleBackColor = true;
+            this.btnCalcularImpuesto.Click += new System.EventHandler(this.btnCalcularImpuesto_Click);
+            // 
+            // txtCantidadImpuesto
+            // 
+            this.txtCantidadImpuesto.Location = new System.Drawing.Point(252, 83);
+            this.txtCantidadImpuesto.Name = "txtCantidadImpuesto";
+            this.txtCantidadImpuesto.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidadImpuesto.TabIndex = 3;
+            // 
+            // lblValorAPagarImpuesto
+            // 
+            this.lblValorAPagarImpuesto.AutoSize = true;
+            this.lblValorAPagarImpuesto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValorAPagarImpuesto.Location = new System.Drawing.Point(163, 145);
+            this.lblValorAPagarImpuesto.Name = "lblValorAPagarImpuesto";
+            this.lblValorAPagarImpuesto.Size = new System.Drawing.Size(163, 20);
+            this.lblValorAPagarImpuesto.TabIndex = 4;
+            this.lblValorAPagarImpuesto.Text = "El valor a pagar es:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(704, 402);
-            this.Controls.Add(this.btnEdan);
-            this.Controls.Add(this.lblEdad);
-            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.lblValorAPagarImpuesto);
+            this.Controls.Add(this.txtCantidadImpuesto);
+            this.Controls.Add(this.btnCalcularImpuesto);
+            this.Controls.Add(this.lblCantidadImpuesto);
+            this.Controls.Add(this.lblTituloImouesto);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -78,9 +103,11 @@ namespace ejerciciosvs
 
         #endregion
 
-        private System.Windows.Forms.MaskedTextBox txtEdad;
-        private System.Windows.Forms.Label lblEdad;
-        private System.Windows.Forms.Button btnEdan;
+        private System.Windows.Forms.Label lblTituloImouesto;
+        private System.Windows.Forms.Label lblCantidadImpuesto;
+        private System.Windows.Forms.Button btnCalcularImpuesto;
+        private System.Windows.Forms.TextBox txtCantidadImpuesto;
+        private System.Windows.Forms.Label lblValorAPagarImpuesto;
     }
 }
 
